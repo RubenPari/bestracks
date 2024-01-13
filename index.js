aff23b27c1f9cd7a2f4640a5c4f4d3a3d57d18e0
RubenPari <rubenpari.developer@gmail.com>
2024-01-13 21:58:29 +0100

    added prettier and eslint

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const Koa = require("koa");
-const Router = require("koa-router");
-const session = require("koa-session");
-const SpotifyWebApi = require("spotify-web-api-node");
-const logger = require("./config/logger");
+const Koa = require('koa');
+const Router = require('koa-router');
+const session = require('koa-session');
+const SpotifyWebApi = require('spotify-web-api-node');
+const logger = require('./config/logger');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = new Koa();
 const router = new Router();
 
-app.keys = ["some secret hurr"];
+app.keys = ['some secret hurr'];
 
 const sessionConfig = {
-  key: "koa.sess",
+  key: 'koa.sess',
   maxAge: 86400000,
   autoCommit: true,
   overwrite: true,
@@ -26,21 +26,21 @@ const sessionConfig = {
 
 app.use(session(sessionConfig, app));
 
-const SCOPES = process.env.SCOPES.split(",");
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const PLAYLIST_TRACKS_TOP_50 = process.env.PLAYLIST_TRACKS_TOP_50;
+const SCOPES = process.env.SCOPES.split(',');
+const { CLIENT_ID } = process.env;
+const { CLIENT_SECRET } = process.env;
+const { REDIRECT_URI } = process.env;
+const { PLAYLIST_TRACKS_TOP_50 } = process.env;
 
 if (
-  !CLIENT_ID ||
-  !CLIENT_SECRET ||
-  !REDIRECT_URI ||
-  !SCOPES ||
-  !PLAYLIST_TRACKS_TOP_50
+  !CLIENT_ID
+  || !CLIENT_SECRET
+  || !REDIRECT_URI
+  || !SCOPES
+  || !PLAYLIST_TRACKS_TOP_50
 ) {
   throw new Error(
-    "Missing environment variables: CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, PLAYLIST_TRACKS_TOP_50"
+    'Missing environment variables: CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, PLAYLIST_TRACKS_TOP_50',
   );
 }
 
@@ -53,11 +53,10 @@ const spotifyApi = new SpotifyWebApi({
 // ### Auth ###
 
 // LOGIN
-router.get("/auth/login", async (ctx) => {
+router.get('/auth/login', async (ctx) => {
   // generate random state
-  const state =
-    Math.random().toString(36).substring(2, 15) +
-    Math.random().toString(36).substring(2, 15);
+  const state = Math.random().toString(36).substring(2, 15)
+    + Math.random().toString(36).substring(2, 15);
 
   // save state to session
   ctx.session.state = state;
@@ -79,15 +78,15 @@ router.get("/auth/login", async (ctx) => {
 });
 
 // CALLBACK
-router.get("/auth/callback", async (ctx) => {
-  const code = ctx.query.code;
-  const state = ctx.query.state;
+router.get('/auth/callback', async (ctx) => {
+  const { code } = ctx.query;
+  const { state } = ctx.query;
 
   // check state and code are defined
   if (!code || !state) {
     ctx.response.status = 400;
     ctx.response.body = {
-      error: "Missing code or state",
+      error: 'Missing code or state',
     };
     return;
   }
@@ -96,7 +95,7 @@ router.get("/auth/callback", async (ctx) => {
   if (state !== ctx.session.state) {
     ctx.response.status = 400;
     ctx.response.body = {
-      error: "Invalid state",
+      error: 'Invalid state',
     };
     return;
   }
@@ -118,29 +117,29 @@ router.get("/auth/callback", async (ctx) => {
 
   // check access token
   if (
-    !dataAccessToken.body["access_token"] ||
-    !dataAccessToken.body["refresh_token"]
+    !dataAccessToken.body.access_token
+    || !dataAccessToken.body.refresh_token
   ) {
     ctx.response.status = 400;
     ctx.response.body = {
-      message: "Error to get access token or refresh token",
+      message: 'Error to get access token or refresh token',
       error: dataAccessToken.toString(),
     };
     return;
   }
 
   // set access token on API
-  spotifyApi.setAccessToken(dataAccessToken.body["access_token"]);
-  spotifyApi.setRefreshToken(dataAccessToken.body["refresh_token"]);
+  spotifyApi.setAccessToken(dataAccessToken.body.access_token);
+  spotifyApi.setRefreshToken(dataAccessToken.body.refresh_token);
 
   ctx.response.status = 200;
   ctx.response.body = {
-    message: "Successfully authenticated",
+    message: 'Successfully authenticated',
   };
 });
 
 // LOGOUT
-router.get("/auth/logout", async (ctx) => {
+router.get('/auth/logout', async (ctx) => {
   ctx.session = null;
 
   spotifyApi.setAccessToken(null);
@@ -148,35 +147,36 @@ router.get("/auth/logout", async (ctx) => {
 
   ctx.response.status = 200;
   ctx.response.body = {
-    message: "Successfully logged out",
+    message: 'Successfully logged out',
   };
 });
 
 // ### User ###
-router.get("/user/top-tracks", async (ctx) => {
-  let topTracks = [];
+router.get('/user/top-tracks', async (ctx) => {
+  const topTracks = [];
   let offset = 0;
   let total = 0;
   const limit = 50;
 
-  logger.info("Inizio recupero delle tracce top.");
+  logger.info('Inizio recupero delle tracce top.');
 
   try {
     let response;
     do {
       logger.info(
-        `Esecuzione della chiamata API per le tracce top, offset: ${offset}`
+        `Esecuzione della chiamata API per le tracce top, offset: ${offset}`,
       );
 
       response = (
+        // eslint-disable-next-line no-await-in-loop
         await spotifyApi.getMyTopTracks({
-          limit: limit,
-          offset: offset,
+          limit,
+          offset,
         })
       ).body;
 
       if (response.total === 0) {
-        throw new Error("No top tracks found");
+        throw new Error('No top tracks found');
       }
 
       topTracks.push(...response.items);
@@ -184,10 +184,10 @@ router.get("/user/top-tracks", async (ctx) => {
       offset += limit;
     } while (offset < total);
 
-    logger.info("Recupero delle tracce top completato.");
+    logger.info('Recupero delle tracce top completato.');
     logger.info(`Tracce recuperate: ${topTracks.length}`);
   } catch (error) {
-    logger.error("Errore durante il recupero delle tracce top:", error.message);
+    logger.error('Errore durante il recupero delle tracce top:', error.message);
 
     ctx.response.status = 400;
     ctx.response.body = {
@@ -198,14 +198,14 @@ router.get("/user/top-tracks", async (ctx) => {
 
   // delete all tracks from playlist
   try {
-    logger.info("Eliminazione di tutte le tracce vecchie dalla playlist.");
+    logger.info('Eliminazione di tutte le tracce vecchie dalla playlist.');
 
     // get all older tracks from playlist
     const oldTracks = (
       await spotifyApi.getPlaylistTracks(PLAYLIST_TRACKS_TOP_50)
     ).body.items;
 
-    if (oldTracks.length === 0) logger.info("Non ci sono tracce da eliminare.");
+    if (oldTracks.length === 0) logger.info('Non ci sono tracce da eliminare.');
     else {
       const tracksToRemove = oldTracks.map((track) => ({
         uri: track.track.uri,
@@ -213,14 +213,14 @@ router.get("/user/top-tracks", async (ctx) => {
 
       const isRemovedTracks = await spotifyApi.removeTracksFromPlaylist(
         PLAYLIST_TRACKS_TOP_50,
-        tracksToRemove
+        tracksToRemove,
       );
 
       if (!isRemovedTracks.body.snapshot_id) {
-        throw new Error("Error to remove tracks from playlist");
+        throw new Error('Error to remove tracks from playlist');
       }
 
-      logger.info("Tracce eliminate.");
+      logger.info('Tracce eliminate.');
     }
   } catch (error) {
     logger.error("Errore durante l'eliminazione delle tracce:", error.message);
@@ -233,27 +233,27 @@ router.get("/user/top-tracks", async (ctx) => {
 
   // add all new tracks to playlist
   try {
-    logger.info("Inserimento di nuove tracce.");
+    logger.info('Inserimento di nuove tracce.');
 
     if (topTracks.length === 0) {
-      logger.info("Non ci sono nuove tracce da inserire.");
+      logger.info('Non ci sono nuove tracce da inserire.');
       return;
     }
 
     const isAddedTracks = await spotifyApi.addTracksToPlaylist(
       PLAYLIST_TRACKS_TOP_50,
-      topTracks.map((track) => track.uri)
+      topTracks.map((track) => track.uri),
     );
 
     if (!isAddedTracks.body.snapshot_id) {
-      throw new Error("Error to add tracks to playlist");
+      throw new Error('Error to add tracks to playlist');
     }
 
-    logger.info("Tracce inserite.");
+    logger.info('Tracce inserite.');
 
-    ctx.response.status = 201
+    ctx.response.status = 201;
     ctx.response.body = {
-      message: "Top tracks added successfully"
+      message: 'Top tracks added successfully',
     };
   } catch (error) {
     logger.error("Errore durante l'inserimento delle tracce:", error.message);
