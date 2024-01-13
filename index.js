2e832eaf350f0aa57e6831e87377e849d8dab1c8
RubenPari <rubenpari.developer@gmail.com>
2024-01-13 12:01:13 +0100

    completed /user/top-tracks endpoint

--- index.js
+++ index.js
@@ -30,10 +30,17 @@ const SCOPES = process.env.SCOPES.split(",");
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const PLAYLIST_TRACKS_TOP_50 = process.env.PLAYLIST_TRACKS_TOP_50;
 
-if (!CLIENT_ID || !CLIENT_SECRET || !REDIRECT_URI || !SCOPES) {
+if (
+  !CLIENT_ID ||
+  !CLIENT_SECRET ||
+  !REDIRECT_URI ||
+  !SCOPES ||
+  !PLAYLIST_TRACKS_TOP_50
+) {
   throw new Error(
-    "Missing environment variables: CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES",
+    "Missing environment variables: CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, PLAYLIST_TRACKS_TOP_50"
   );
 }
 
@@ -149,41 +156,113 @@ router.get("/auth/logout", async (ctx) => {
 router.get("/user/top-tracks", async (ctx) => {
   let topTracks = [];
   let offset = 0;
+  let total = 0;
   const limit = 50;
 
-  logger.info('Inizio recupero delle tracce top.');
+  logger.info("Inizio recupero delle tracce top.");
 
   try {
     let response;
     do {
-      logger.info(`Esecuzione della chiamata API per le tracce top, offset: ${offset}`);
+      logger.info(
+        `Esecuzione della chiamata API per le tracce top, offset: ${offset}`
+      );
 
-      response = (await spotifyApi.getMyTopTracks({
-        limit: limit,
-        offset: offset,
-      })).body;
+      response = (
+        await spotifyApi.getMyTopTracks({
+          limit: limit,
+          offset: offset,
+        })
+      ).body;
+
+      if (response.total === 0) {
+        throw new Error("No top tracks found");
+      }
 
       topTracks.push(...response.items);
+      total = response.total;
       offset += limit;
+    } while (offset < total);
 
-    } while (response.items.length === limit);
-
-    logger.info('Recupero delle tracce top completato.');
+    logger.info("Recupero delle tracce top completato.");
     logger.info(`Tracce recuperate: ${topTracks.length}`);
-
-  } catch(error) {
+  } catch (error) {
     logger.error("Errore durante il recupero delle tracce top:", error.message);
 
     ctx.response.status = 400;
     ctx.response.body = {
-      message: "Error to get top tracks",
       error: error.message,
     };
     return;
   }
 
-  ctx.response.status = 200;
-  ctx.response.body = topTracks;
+  // delete all tracks from playlist
+  try {
+    logger.info("Eliminazione di tutte le tracce vecchie dalla playlist.");
+
+    // get all older tracks from playlist
+    const oldTracks = (
+      await spotifyApi.getPlaylistTracks(PLAYLIST_TRACKS_TOP_50)
+    ).body.items;
+
+    if (oldTracks.length === 0) logger.info("Non ci sono tracce da eliminare.");
+    else {
+      const tracksToRemove = oldTracks.map((track) => ({
+        uri: track.track.uri,
+      }));
+
+      const isRemovedTracks = await spotifyApi.removeTracksFromPlaylist(
+        PLAYLIST_TRACKS_TOP_50,
+        tracksToRemove
+      );
+
+      if (!isRemovedTracks.body.snapshot_id) {
+        throw new Error("Error to remove tracks from playlist");
+      }
+
+      logger.info("Tracce eliminate.");
+    }
+  } catch (error) {
+    logger.error("Errore durante l'eliminazione delle tracce:", error.message);
+
+    ctx.response.status = 400;
+    ctx.response.body = {
+      error: error.message,
+    };
+  }
+
+  // add all new tracks to playlist
+  try {
+    logger.info("Inserimento di nuove tracce.");
+
+    if (topTracks.length === 0) {
+      logger.info("Non ci sono nuove tracce da inserire.");
+      return;
+    }
+
+    const isAddedTracks = await spotifyApi.addTracksToPlaylist(
+      PLAYLIST_TRACKS_TOP_50,
+      topTracks.map((track) => track.uri)
+    );
+
+    if (!isAddedTracks.body.snapshot_id) {
+      throw new Error("Error to add tracks to playlist");
+    }
+
+    logger.info("Tracce inserite.");
+
+    ctx.response.status = 201
+    ctx.response.body = {
+      message: "Top tracks added successfully"
+    };
+  } catch (error) {
+    logger.error("Errore durante l'inserimento delle tracce:", error.message);
+
+    ctx.response.status = 400;
+    ctx.response.body = {
+      error: error.message,
+    };
+  }
 });
 
 app.use(router.routes());
