f8a1fa264e53eb4ef91a1a9246532ac0e43d030f
RubenPari <rubenpari.developer@gmail.com>
2024-01-14 12:12:47 +0100

    mapped router.js

---
+++ src/router/router.js
@@ -0,0 +1,20 @@
+const Router = require('koa-router');
+
+const authController = require('../controller/authController');
+const userController = require('../controller/userController');
+
+const authRouter = new Router({ prefix: '/auth' });
+const userRouter = new Router({ prefix: '/user' });
+
+// ### AUTH ###
+authRouter.get('/login', authController.login);
+authRouter.get('/callback', authController.callback);
+authRouter.get('/logout', authController.logout);
+
+// ### USER ###
+userRouter.get('/top-tracks', userController.getTopTracks);
+
+module.exports = {
+  authRouter,
+  userRouter,
+};
